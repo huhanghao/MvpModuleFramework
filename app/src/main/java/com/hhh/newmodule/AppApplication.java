1012c57692fcced0666b49894de06f1da41d16eb
huhanghao <517072971@qq.com>
2018-07-20 13:10:19 +0800

    1、改造了下网络

--- app/src/main/java/com/hhh/newmodule/AppApplication.java
+++ app/src/main/java/com/hhh/newmodule/AppApplication.java
@@ -5,17 +5,13 @@ import android.content.Context;
 import android.support.multidex.MultiDex;
 
 import com.alibaba.android.arouter.launcher.ARouter;
-import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.Utils;
-import com.hhh.lib_api.interfaces.IOnNetEventListener;
-import com.hhh.lib_api.services.impl.IHttpClientImp;
-import com.hhh.lib_api.token.TokenManager;
 
 /**
  * Created by nova on 18/02/2018.
  */
 
-public class RMApplication extends Application {
+public class AppApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -23,23 +19,6 @@ public class RMApplication extends Application {
         ARouter.openDebug();   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
         ARouter.init(this);
         Utils.init(this);
-
-        IHttpClientImp.setOnEventListener(new IOnNetEventListener() {
-            @Override
-            public void onAuthError() {
-                // to do
-                // need test
-                ActivityUtils.finishAllActivities();
-                ARouter.getInstance().build("/login/login").navigation();
-                TokenManager.getInstance().setToken(null);
-
-            }
-
-            @Override
-            public void onApiSuccess(String path) {
-
-            }
-        });
     }
 
     @Override
