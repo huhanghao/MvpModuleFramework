92374e81f2c542d97be31333804d28618c7392dd
huhanghao <517072971@qq.com>
2018-09-20 17:27:33 +0800

    1、重新设置了网络请求的数据转化
    下一步：
    暂无

--- lib_api/src/main/java/com/hhh/lib_api/DataFlatMap.java
+++ lib_api/src/main/java/com/hhh/lib_api/DataFlatMap.java
@@ -24,26 +24,27 @@ import okhttp3.ResponseBody;
 
 public class DataFlatMap {
 
+
     /**
      * 中间数据转换
      * @return
      */
-    public static Function<ResponseBody, Observable<JSONObject>> middleDataFlatMap() {
-        return new Function<ResponseBody, Observable<JSONObject>>() {
-           
-            public Observable<JSONObject> apply(ResponseBody body) throws Exception {
+    public static Function<ResponseBody, Observable<String>> middleDataFlatMap() {
+        return new Function<ResponseBody, Observable<String>>() {
+
+            public Observable<String> apply(ResponseBody body) throws Exception {
                 String json = body.string();
                 LogUtils.d(json);
 
                 JSONObject jsonObject = new JSONObject(json);
 
-                int code = jsonObject.optInt("code", HttpCode.CODE_FAILED);
+                int code = jsonObject.optInt("error", HttpCode.CODE_FAILED);
                 if (code == HttpCode.CODE_SUCCESS) {
-                    JSONObject jsonData = jsonObject.getJSONObject("data");
-                    return Observable.just(jsonData);
+                    String jsonStr = jsonObject.getString("data");
+                    return Observable.just(jsonStr);
                 } else {
-                    final String msg = jsonObject.optString("msg");
-                    APIError apiError = new APIError(msg.length() == 0 ? "" : msg, code);
+                    final String msg = jsonObject.optString("message");
+                    APIError apiError = new APIError(msg.length() == 0 ? "网络访问错误" : msg, code);
                     return Observable.error(apiError);
                 }
             }
@@ -79,16 +80,16 @@ public class DataFlatMap {
      * @param <T>
      * @return
      */
-    public static <T>Function<JSONObject, ObservableSource<T>> data2ObjectFlatMap(final Class<T> cls) {
-        return new Function<JSONObject, ObservableSource<T>>() {
+    public static <T>Function<String, ObservableSource<T>> data2ObjectFlatMap(final Class<T> cls) {
+        return new Function<String, ObservableSource<T>>() {
             @Override
-            public ObservableSource<T> apply(JSONObject jsonObject) throws Exception {
+            public ObservableSource<T> apply(String jsonStr) throws Exception {
                 try {
-                    String json = jsonObject.toString();
+                    String json = jsonStr;
                     T data = new GsonBuilder().create().fromJson(json, cls);
                     return Observable.just(data);
                 }catch (Exception e) {
-                    LogUtils.e("data2ObjectFlatMap", "Exception");
+                    LogUtils.e("data2ObjectFlatMap", e.toString());
                     return Observable.just(cls.newInstance());
                 }
             }
@@ -101,12 +102,12 @@ public class DataFlatMap {
      * @param type
      * @return
      */
-    public static Function<JSONObject, ObservableSource<? extends List>> data2ListFlatMap(final Type type) {
-        return new Function<JSONObject, ObservableSource<? extends List>>() {
+    public static Function<String, ObservableSource<? extends List>> data2ListFlatMap(final Type type) {
+        return new Function<String, ObservableSource<? extends List>>() {
             @Override
-            public ObservableSource<? extends List> apply(@NonNull JSONObject jsonObject) throws Exception {
+            public ObservableSource<? extends List> apply(@NonNull String jsonStr) throws Exception {
                 try {
-                    String json = jsonObject.toString();
+                    String json = jsonStr;
                     List list = new GsonBuilder().create().fromJson(json, type);
                     return Observable.just(list);
                 }catch (Exception e) {
@@ -121,10 +122,10 @@ public class DataFlatMap {
      * 只返会boolean
      * @return
      */
-    public static Function<JSONObject, ObservableSource<Boolean>> data2BooleanFlatMap() {
-        return new Function<JSONObject, ObservableSource<Boolean>>() {
+    public static Function<String, ObservableSource<Boolean>> data2BooleanFlatMap() {
+        return new Function<String, ObservableSource<Boolean>>() {
             @Override
-            public ObservableSource<Boolean> apply(@NonNull JSONObject body) throws Exception {
+            public ObservableSource<Boolean> apply(@NonNull String body) throws Exception {
                 return Observable.just(true);
             }
         };
@@ -134,14 +135,15 @@ public class DataFlatMap {
      * 直接返回String
      * @return
      */
-    public static Function<JSONObject, ObservableSource<String>> data2StringFlatMap() {
-        return new Function<JSONObject, ObservableSource<String>>() {
+    public static Function<String, ObservableSource<String>> data2StringFlatMap() {
+        return new Function<String, ObservableSource<String>>() {
             @Override
-            public ObservableSource<String> apply(@NonNull JSONObject body) throws Exception {
+            public ObservableSource<String> apply(@NonNull String body) throws Exception {
                 return Observable.just(body.toString());
             }
         };
     }
+
 
 
 }
